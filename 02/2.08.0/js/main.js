c1572e716ca43ca4403aaad97525dd9626c62fa8
Sabine Maennel <sabine.maennel@gmail.com>
2020-03-19 10:38:46 +0100

    add 2nd task: simple bar plot

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,31 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+d3.json("data/buildings.json").then(function(data){
+    data.forEach(function(d){
+        console.log(d);
+        d.height = +d.height;
+    });
+
+    var svg = d3.select("#chart-area").append("svg")
+        .attr("width", 1000)
+        .attr("height", 400);
+
+    var rectangles = svg.selectAll("rect")
+        .data(data);
+
+    rectangles.enter()
+        .append("rect")
+            .attr("x", function(d, i){
+                return (i * 50) + 25;
+            })
+            .attr("y", 25)
+            .attr("height", function(d){
+                return d.height;
+            })
+            .attr("width", 25)
+            .attr("fill", "blue");
+}).catch(function(error){
+    console.log(error);
+});
